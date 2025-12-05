59bd46afa61f079a6b38f1f10ff89aa0abbc6dea
Pen Diaz <stephendiaz.syv@gmail.com>
2025-12-05 09:04:09 +0800

    temp: fixed web directory

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,16 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from "@/pages/Home.jsx"
-import MainTest from "@/pages/MainTest.jsx"
+import Dashboard from "@/pages/Dashboard.jsx"
 import Login from "@/pages/Login.jsx"
 import SignUp from "@/pages/SignUp.jsx"
-import MainPage from "@/pages/MainPage.jsx"
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/test" element={<MainTest />} />
-      <Route path="/main" element={<MainPage />} />
+      <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/login" element={<Login />} />
       <Route path="/sign-up" element={<SignUp />} />
     </Routes>
