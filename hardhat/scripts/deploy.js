7501d35d1f402e1ad35ddcd0e50fd7a1da024408
Ericka Brudo <erickabrudo2@gmail.com>
2025-12-16 09:22:03 +0800

    Lock.js fix

--- hardhat/scripts/deploy.js
+++ hardhat/scripts/deploy.js
@@ -5,7 +5,6 @@ dotenv.config();
 const { ethers } = hardhat;
 
 async function main() {
-  // Connect to the configured network
   const networkUrl = process.env.RPC_URL;
   const privateKey = process.env.PRIVATE_KEY;
 
@@ -15,21 +14,21 @@ async function main() {
 
   console.log("Using network RPC:", networkUrl);
 
-  // Manually create a signer using the private key for deployment
+
   const provider = new ethers.JsonRpcProvider(networkUrl);
   const deployer = new ethers.Wallet(privateKey, provider);
 
   console.log("Deploying with account:", deployer.address);
 
-  // Deploy the contract
+
   const SimpleStorage = await ethers.getContractFactory("SimpleStorage", deployer);
   const simpleStorage = await SimpleStorage.deploy();
 
-  await simpleStorage.waitForDeployment(); // Wait until deployed
+  await simpleStorage.waitForDeployment(); 
   console.log("SimpleStorage deployed to:", simpleStorage.target);
 }
 
-// Run the deployment script
+
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
